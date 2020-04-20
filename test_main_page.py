9ea68accd2eb471d4b3bdcd830668f8dfd54a616
woxdark <woxdark@mail.ru>
2020-04-20 22:45:20 +0300

    go_to_login_page реализована в теле теста

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,16 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.locators import MainPageLocators
+
+def go_to_login_page(self):
+    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
